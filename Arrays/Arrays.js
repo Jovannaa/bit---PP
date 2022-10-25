9bb049e0cbfe2260e56b51d4176da1069c1d2d85
Jovana <jovanamatic3@gmail.com>
2022-10-25 17:52:29 +0200

    Finished arrays exercise

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -9,4 +9,38 @@ console.log(months [5], months [9], months [0])
 2. Write an arrays of days in a week. Using console.log display Sunday from the array.
 */
 
-var arr = ["Monday", "Tuesday", "Wednesday",]
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+console.log(days [6]);
+
+/*
+3. Print all negative elements from the array [2, -4, 5, -2, -11].
+*/
+
+var elements = [2, -4, 5, -2, -11];
+
+console.log(elements [1], elements[3], elements[4]);
+
+
+/*
+4. Print all elements with indices divisible by 3 from the array [5, 15, -5, 20, 12, 18, 72, 14, 9].
+*/
+
+var elements = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+
+console.log(elements [1], elements [4], elements [5], elements [6], elements [8]);
+
+
+/*
+5. What is the index of number 24 in the following array?
+[[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+Using console.log:
+ ● Display the 3rd element of the array,
+ ● Display the 2nd element of the 3rd element.
+ */
+
+ var arr = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+ console.log(arr [0][3]);
+ console.log(arr [3][2]);
+
